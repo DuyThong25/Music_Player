726b55fc1b0f726da50991b07582bc5bfcbcd9c8
DuyThong25 <duythong2505@gmail.com>
2023-08-04 20:18:25 +0700

    Update handle event play/pause audio

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,28 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const heading = $("header h2");
+const singer = $("header h3");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const currentAudioTime = $(".viewTime-progress");
+const durationAudioTime = $(".viewTime-duration");
+const playBtn = $(".btn-toggle-play");
+const player = $(".player");
+const cd = $(".cd");
+const cdWidth = cd.offsetWidth;
+const header = $(".info header");
+
+// Hàm để định dạng thời gian thành chuỗi phút:giây
+function formatTime(timeInSeconds) {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = Math.floor(timeInSeconds % 60);
+  const formattedTime = `${minutes}:${String(seconds).padStart(2, "0")}`;
+  return formattedTime;
+}
 const app = {
+  currentIndex: 0,
+  ifPlaying: false,
   songs: [
     {
       name: "Nụ hôn bisou",
@@ -64,7 +85,7 @@ const app = {
       image: "../assets/images/anhlaai.png",
     },
   ],
-  // 1. Render ra list nhạc
+  // 1. Render ra list nhạc -- Render Playlists
   render: function () {
     const htmls = this.songs.map((song) => {
       return `
@@ -86,32 +107,69 @@ const app = {
     $(".playlist").innerHTML = htmls.join("");
   },
   //Xử lý sự kiện
-  handleEvent: function() {
-    
-    //Phóng to thu nhỏ đĩa
-    const cd = $('.cd');
-    const header = $('.info header');
-    const cdWidth = cd.offsetWidth;
-    document.onscroll = function() {
-      const scrolTop = window.scrollY || document.documentElement.scrollTop ;
-      const newWithCD = cdWidth - scrolTop;
-      if(newWithCD > 0) {
+  handleEvents: function () {
+    //Phóng to thu nhỏ đĩa -- Scroll Top
+    document.onscroll = function () {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop; // Một số trình duyệt chỉ hổ trợ 1 trong hai nên nếu k có thằng kia thì lấy thằng nọ
+      const newWithCD = cdWidth - scrollTop;
+      const opacityCD = newWithCD / cdWidth;
+      if (newWithCD > 0) {
         cd.style.width = newWithCD + "px";
-      } else{
-        cd.style.width = 0 ;
-        header.style.margin = "auto";
-        cd.style.margin = 0;
-
+        cd.style.opacity = opacityCD;
+      } else {
+        cd.style.width = 0;
+        // header.style.margin = "auto";
+        // cd.style.margin = 0;
       }
-      // console.log([newWithCD])
-      // console.log(Math.round(scrolY));
-
+    };
+    //Xử lý sự kiện nhấn Play - Pause
+    playBtn.onclick = () => {
+      if (!app.isPlaying) {
+        app.isPlaying = true;
+        audio.play();
+        player.classList.add("playing");
+      } else {
+        app.isPlaying = false;
+        audio.pause();
+        player.classList.remove("playing");
+      }
+    };
+    //Xử lý sự kiện khi audio đang chạy
+    audio.ontimeupdate = () => {
+      currentAudioTime.textContent = formatTime(audio.currentTime);
+      durationAudioTime.textContent = formatTime(audio.duration - audio.currentTime);      
     }
   },
+  defineProperties: function () {
+    Object.defineProperty(this, "CurrentSong", {
+      get: () => {
+        return this.songs[this.currentIndex];
+      },
+    });
+  },
+  loadCurrentSong: function () {
+    heading.textContent = this.CurrentSong.name;
+    singer.textContent = this.CurrentSong.singer;
+    cdThumb.style.backgroundImage = `url(${this.CurrentSong.image})`;
+    audio.src = this.CurrentSong.path;
+    // Phải thêm event loadedmetadata để kiểm tra khi nào dữ liệu audio được tải hoàn toàn thì mới thêm vô
+    audio.addEventListener('loadedmetadata', () => {
+      // currentAudioTime.textContent = formatTime(audio.buffered.start(0));
+      durationAudioTime.textContent = formatTime(audio.duration);
+    });
 
+  },
   start: function () {
+    //Định nghĩa các thuộc tính cho Object
+    this.defineProperties();
+
+    //handle Event
+    this.handleEvents();
+    //Load current song
+    this.loadCurrentSong();
+    //Render List
     this.render();
-    this.handleEvent();
+
   },
 };
 
